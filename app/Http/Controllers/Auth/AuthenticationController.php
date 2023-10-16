d22d1213f2d5ff299f68eadc9397d1537d88fd69
Wiyuda <wiyudapratama05@gmail.com>
2023-10-16 19:41:01 +0700

    create authentication user

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function authentication(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->route('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'username' => 'Username atau password tidak sesuai.',
+        ])->onlyInput('username');
+    }
+}
